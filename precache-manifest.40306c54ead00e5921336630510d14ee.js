d322ce0771c7180f96a8dc96d684af7df8e4473e
thebestteamaker@gmail.com <thebestteamaker@gmail.com>
2019-12-26 13:34:21 -0800

    init commit

---
+++ precache-manifest.40306c54ead00e5921336630510d14ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ef94ef03784e70516f79b797a5b0f1",
+    "url": "/reactapptutorial/index.html"
+  },
+  {
+    "revision": "56b2d3cdd97ca7671b43",
+    "url": "/reactapptutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "17563ff368e44eaddec4",
+    "url": "/reactapptutorial/static/js/2.e099ff57.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactapptutorial/static/js/2.e099ff57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56b2d3cdd97ca7671b43",
+    "url": "/reactapptutorial/static/js/main.984e2c1d.chunk.js"
+  },
+  {
+    "revision": "19eb24648f6cb9b14586",
+    "url": "/reactapptutorial/static/js/runtime-main.f0c36fd7.js"
+  }
+]);
